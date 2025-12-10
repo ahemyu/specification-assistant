4d3ed4ceabdb51c72e7f66e1e53913627e07e2b2
Emre <emre.goekcek@trench-group.com>
2025-12-10 12:22:02 +0100

    fix: move port from 8080 to 80 (for webserver)

--- src/pdf_reader/main.py
+++ src/pdf_reader/main.py
@@ -10,4 +10,4 @@ from backend.app import app  # noqa: F401
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=False)
